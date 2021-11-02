011364c563f274f8e8ad9e8dcfbe8b9dc8b458cf
Albert Pangestu <stamina.left@gmail.com>
2021-11-02 15:12:54 +0700

    New protocol named 'ReportReader' with type of ObservableObject.

--- ExpenseTracker/Protocols/ReportReader.swift
+++ ExpenseTracker/Protocols/ReportReader.swift
@@ -30,4 +30,8 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import Foundation
+import Combine
+
+protocol ReportReader: ObservableObject {
+  <#requirements#>
+}
