4859eefeadd87adfd1dd945fc7a2401db8e20483
Albert Pangestu <stamina.left@gmail.com>
2021-11-02 11:05:30 +0700

    Added List outside ForEach.

--- ExpenseTracker/Views/ContentView.swift
+++ ExpenseTracker/Views/ContentView.swift
@@ -35,8 +35,10 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     NavigationView {
-      ForEach(ReportRange.allCases, id: \.self) { value in
-        NavigationLink(value.rawValue, destination: expensesView(for: value).navigationTitle(value.rawValue))
+      List {
+        ForEach(ReportRange.allCases, id: \.self) { value in
+          NavigationLink(value.rawValue, destination: expensesView(for: value).navigationTitle(value.rawValue))
+        }
       }
       .navigationTitle("Reports")
     }
