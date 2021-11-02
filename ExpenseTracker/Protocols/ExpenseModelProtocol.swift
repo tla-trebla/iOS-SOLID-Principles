f6ff271cec7f8394868da39376bc9b04eb3333ff
Albert Pangestu <stamina.left@gmail.com>
2021-11-02 11:22:23 +0700

    New protocol named ExpenseProtocol with optional properties title, comment, date, and id, and required properties price.

--- ExpenseTracker/Protocols/ExpenseModelProtocol.swift
+++ ExpenseTracker/Protocols/ExpenseModelProtocol.swift
@@ -31,3 +31,11 @@
 /// THE SOFTWARE.
 
 import Foundation
+
+protocol ExpenseProtocol {
+  var title: String? { get }
+  var price: Double { get }
+  var comment: String? { get }
+  var date: Date? { get }
+  var id: UUID? { get }
+}
