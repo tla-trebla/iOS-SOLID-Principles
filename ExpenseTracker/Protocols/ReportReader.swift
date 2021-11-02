da0820c91ce9d1b5934f552b7647b53860dfb353
Albert Pangestu <stamina.left@gmail.com>
2021-11-02 17:42:37 +0700

    Removed saveEntry method because it already exists at SaveEntryProtocol.

--- ExpenseTracker/Protocols/ReportReader.swift
+++ ExpenseTracker/Protocols/ReportReader.swift
@@ -35,7 +35,6 @@ import Foundation
 
 class ReportReader: ObservableObject {
   @Published var currentEntries: [ExpenseModelProtocol] = []
-  func saveEntry(title: String, price: Double, date: Date, comment: String) {}
   func prepare() {
     assertionFailure("Missing override: Please override this method in the subclass")
   }
