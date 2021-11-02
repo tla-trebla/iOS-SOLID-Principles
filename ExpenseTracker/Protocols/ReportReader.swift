ba1a2805aba26834123a7480c33080169662e40b
Albert Pangestu <stamina.left@gmail.com>
2021-11-02 15:43:01 +0700

    Changed into class type from prototype, adds implementation for methods, and add value for the property.

--- ExpenseTracker/Protocols/ReportReader.swift
+++ ExpenseTracker/Protocols/ReportReader.swift
@@ -33,8 +33,10 @@
 import Combine
 import Foundation
 
-protocol ReportReader: ObservableObject {
-  @Published var currentEntries: [ExpenseModelProtocol] { get }
-  func saveEntry(title: String, price: Double, date: Date, comment: String)
-  func prepare()
+class ReportReader: ObservableObject {
+  @Published var currentEntries: [ExpenseModelProtocol] = []
+  func saveEntry(title: String, price: Double, date: Date, comment: String) {}
+  func prepare() {
+    assertionFailure("Missing override: Please override this method in the subclass")
+  }
 }
